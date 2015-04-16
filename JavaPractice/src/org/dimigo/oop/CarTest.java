3149a807abcec76f73e8d392ae28a09a86719ff9
Leehankyeol <Leehankyeol@172.17.1.134>
2015-04-16 09:27:03 +0900

    실습과제 7-2

--- JavaPractice/src/org/dimigo/oop/CarTest.java
+++ JavaPractice/src/org/dimigo/oop/CarTest.java
@@ -3,9 +3,10 @@ package org.dimigo.oop;
 public class CarTest {
 	
 	public static void main(String[] args) {
-		Car car1 = new Car();
-		Car car2 = new Car();
-		Car car3 = new Car();
+		Car2 car1 = new Car2("현대자동차","제네시스","검정색",225,50000000);
+		Car2 car2 = new Car2("기아자동차","K7","흰색",246,40000000);
+		Car2 car3 = new Car2("삼성자동차","SM7","회색",200,38000000);
+		
 		
 		System.out.println("<< 자동차 목록 >>");
 		System.out.println("제조사명 : " + car1.getCompany());
@@ -15,11 +16,6 @@ public class CarTest {
 		System.out.println("가격 : " + String.format("%,d",car1.getPrice()) + "원");
 		System.out.println("");
 		
-		car2.setCompany("기아자동차");
-		car2.setModel("K7");
-		car2.setColor("흰색");
-		car2.setMaxSpeed(246);
-		car2.setPrice(40000000);
 		
 		System.out.println("제조사명 : " + car2.getCompany());
 		System.out.println("모델명 : " + car2.getModel());
@@ -28,11 +24,6 @@ public class CarTest {
 		System.out.println("가격 : " + String.format("%,d",car2.getPrice()) + "원");
 		System.out.println("");
 		
-		car3.setCompany("삼성자동차");
-		car3.setModel("SM7");
-		car3.setColor("회색");
-		car3.setMaxSpeed(200);
-		car3.setPrice(38000000);
 		
 		System.out.println("제조사명 : " + car3.getCompany());
 		System.out.println("모델명 : " + car3.getModel());
@@ -40,5 +31,4 @@ public class CarTest {
 		System.out.println("최대속도 : " + car3.getMaxSpeed() + "km");
 		System.out.println("가격 : " + String.format("%,d",car3.getPrice()) + "원");
 	}
-
 }
