68606e7fe4345b0f66297c59058f88fb9f4d0773
Leehankyeol <Leehankyeol@172.17.2.64>
2015-05-14 09:17:15 +0900

    8번째 실습과

---
+++ JavaPractice/src/org/dimigo/oop/Question.java
@@ -0,0 +1,23 @@
+package org.dimigo.oop;
+
+import java.util.Scanner;
+
+public class Question {
+	public static void main(String[] args) {
+		System.out.println("가장 좋아하는 가수는?");
+		Scanner scanner = new Scanner(System.in);
+		String answer = scanner.nextLine();
+		if(answer.equals("빅뱅")) System.out.println("정답입니다.");
+		else System.out.println("틀렸습니다.");
+		
+		System.out.println("가장 좋아하는 배우는?");
+		String answer2 = scanner.nextLine();
+		if(answer2.equals("조인성")) System.out.println("정답입니다.");
+		else System.out.println("틀렸습니다.");
+		
+		System.out.println("가장 좋아하는 과목은?");
+		String answer3 = scanner.nextLine();
+		if(answer3.equals("자바")) System.out.println("정답입니다.");
+		else System.out.println("틀렸습니다.");
+	}
+}
